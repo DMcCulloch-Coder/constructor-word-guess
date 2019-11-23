f657d303d1fb0bdd94c9336a10bf729f475751cb
David <DMcCulloch86@gmail.com>
2019-11-22 21:24:26 -0500

    fixed bug where spaces were not registering as spaces in letter.js

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const chalk = require('chalk');
 
 const Word = require('./word');
 
+const wordBank = ['']
 
 
 
 
-console.log(chalk.bgCyan.blackBright.bold("Hello World!"))
+
+console.log(chalk.bgCyan.blackBright.bold("Hello World!")) //test
 
 
-
-let temp = new Word("elephant"); //test
+let guess = process.argv[2]; //test
+let temp = new Word("elephant gun"); //test
 temp.initArray(); //test
-temp.checkWord('e') //test
+temp.checkWord(guess) //test
 console.log(temp.word); //test
 console.log(temp.displayWord()); //test
